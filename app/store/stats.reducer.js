5f503c555f79cef6dec8711e5fb9fd22ec264ded
Wes Johnson <swj@wesquire.ca>
2016-04-02 15:08:10 -0400

    :lipstick: more prominent play count bars in playlist

--- app/store/stats.reducer.js
+++ app/store/stats.reducer.js
@@ -1,13 +1,18 @@
 const storage = require('./storage')
 const mapActions = require('../utils/mapActions')
 const actions = require('./stats.actions')
+const values = require('lodash.values')
 
 const statsKey = 'playCounts'
 
 var initialState = storage.read(statsKey, {
   songCounts: {},
-  totalCount: 0
+  maxCount: 0
 })
+
+if (!initialState.maxCount) {
+  initialState.maxCount = Math.max.apply(Math, values(initialState.songCounts))
+}
 
 function reducerFn (state = initialState, action) {
   var newState = Object.assign({}, state)
@@ -17,7 +22,9 @@ function reducerFn (state = initialState, action) {
     case 'add count':
       if (!songCounts[action.id]) songCounts[action.id] = 0
       songCounts[action.id]++
-      newState.totalCount++
+      if (state.songCounts[action.id] > newState.maxCount) {
+        newState.maxCount++
+      }
       storage.write(statsKey, newState)
       break
   }
