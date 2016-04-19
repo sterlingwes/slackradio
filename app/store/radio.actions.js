0430abcf37aa9beaa7b98a8fb929c9bb9e7d3f9c
Wes Johnson <swj@wesquire.ca>
2016-04-19 18:43:24 -0400

    support for push events for new playlists

--- app/store/radio.actions.js
+++ app/store/radio.actions.js
@@ -1,5 +1,9 @@
 module.exports = {
   receivedPlaylists: function (playlists) {
     return { type: 'received playlists', lists: playlists }
+  },
+
+  playlistsAdded: function (playlists) {
+    return { type: 'received additional playlists', lists: playlists }
   }
 }
--- app/store/radio.reducer.js
+++ app/store/radio.reducer.js
@@ -1,18 +1,28 @@
 const mapActions = require('../utils/mapActions')
 const actions = require('./radio.actions')
+const _ = require('lodash')
 
 var initialState = {}
+
+function groupPlaylists (state, lists) {
+  state = lists.reduce((hash, station) => {
+    var team = hash[station.team_domain] = hash[station.team_domain] || []
+    team.push(station)
+    return hash
+  }, state)
+  state._length = Object.keys(state).length
+  return state
+}
 
 function reducerFn (state = initialState, action) {
 
   switch (action.type) {
     case 'received playlists':
-      state = action.lists.data.reduce((hash, station) => {
-        var team = hash[station.team_domain] = hash[station.team_domain] || []
-        team.push(station)
-        return hash
-      }, {})
-      state._length = Object.keys(state).length
+      state = groupPlaylists(state, action.lists.data)
+      break
+
+    case 'received additional playlists':
+      state = groupPlaylists(_.extend({}, state), action.lists)
       break
   }
 
